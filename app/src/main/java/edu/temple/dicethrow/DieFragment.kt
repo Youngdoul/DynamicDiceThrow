53408989089f655c416cb8fa0b4be5d1b404a949
Youngdoul <157045613+Youngdoul@users.noreply.github.com>
2024-10-30 16:13:50 -0400

    Update DieFragment.kt

--- app/src/main/java/edu/temple/dicethrow/DieFragment.kt
+++ app/src/main/java/edu/temple/dicethrow/DieFragment.kt
@@ -5,36 +5,48 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
-import java.util.Locale
 import kotlin.random.Random
 
-class DieFragment : Fragment() {
+const val DIE_SIDES = "dIcE_SiDeS"
 
-    private val dieViewModel: DieViewModel by lazy {
-        ViewModelProvider(requireActivity())[DieViewModel::class.java]
+class DiceFragment : Fragment() {
+    private var sides: Int? = null
+
+    // ViewModel declaration
+    private lateinit var viewModel: DiceViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Activity-scoped ViewModel initialization
+        viewModel = ViewModelProvider(requireActivity())[DiceViewModel::class.java]
+        arguments?.let {
+            sides = it.getInt(DIE_SIDES)
+        }
     }
-
-    private lateinit var dieTextView: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_die, container, false).apply {
-            dieTextView = findViewById(R.id.dieTextView)
+        return inflater.inflate(R.layout.fragment_dice, container, false).apply {
+            findViewById<Button>(R.id.rollButton).setOnClickListener {
+
+                viewModel.setselectnum( (Random.nextInt(sides!!) + 1))
+            }
         }
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    companion object {
 
-        dieViewModel.getDieRoll().observe(viewLifecycleOwner) {
-            dieTextView.text = it.toString()
-        }
-
+        @JvmStatic
+        fun newInstance(sides: Int) =
+            DiceFragment().apply {
+                arguments = Bundle().apply {
+                    putInt(DIE_SIDES, sides)
+                }
+            }
     }
-
-}
